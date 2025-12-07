d9297b1bd3fb191138b940452da41d4400228785
Yuhao <hello@chenyuhao.me>
2025-12-07 00:09:12 -0800

    basic data structure implementation

---
+++ memory.h
@@ -0,0 +1,17 @@
+#ifndef clox_memory_h
+#define clox_memory_h
+
+#include "common.h"
+
+#define GROW_CAPACITY(capacity) \
+  ((capacity) < 8 ? 8 : (capacity) * 2)
+
+#define GROW_ARRAY(type, pointer, oldCount, newCount) \
+  ((type*)realloc(pointer, (newCount) * sizeof(type)))
+
+#define FREE_ARRAY(type, pointer, oldCount) \
+  reallocate(pointer, sizeof(type) * (oldCount), 0)
+
+void* reallocate(void* pointer, size_t oldSize, size_t newSize);
+
+#endif
