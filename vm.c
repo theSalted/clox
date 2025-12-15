43dd9e6ff1f81b5eee8f7a8dd0051118ef35d550
Yuhao <hello@chenyuhao.me>
2025-12-15 01:42:33 -0800

    a virtual machine

--- vm.c
+++ vm.c
@@ -26,6 +26,13 @@ Value pop() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+  do { \
+    Value b = pop(); \
+    Value a = pop(); \
+    push(a op b); \
+  } while (false)
+
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
     printf("          ");
@@ -44,6 +51,10 @@ static InterpretResult run() {
       push(constant);
       break;
     }
+    case OP_ADD:      BINARY_OP(+); break;
+    case OP_SUBTRACT: BINARY_OP(-); break;
+    case OP_MULTIPLY: BINARY_OP(*); break;
+    case OP_DIVIDE:   BINARY_OP(/); break;
     case OP_NEGATE: push(-pop()); break;
     case OP_RETURN: {
       printValue(pop());
@@ -55,6 +66,7 @@ static InterpretResult run() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk) {
